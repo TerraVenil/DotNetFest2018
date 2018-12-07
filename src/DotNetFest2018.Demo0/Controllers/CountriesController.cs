88f4750b37071cf31e9637b7d01160a43b9677df
Alexey Borodenko <kosunix@gmail.com>
2018-12-07 11:57:18 +0200

    Add example with integration IOptions to .NET Framework.

---
+++ src/DotNetFest2018.Demo0/Controllers/CountriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DotNetFest2018.Demo0.Controllers
+{
+    public class CountriesController : ApiController
+    {
+        public CountriesController()
+        {
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new object[] { (code: "UK", name: "United Kingdom"), (code: "USA", name: "United States") };
+        }
+    }
+}
---
+++ src/DotNetFest2018.Demo0/Options/CountryOptions.cs
@@ -0,0 +1,9 @@
+namespace DotNetFest2018.Demo0.Options
+{
+    public class CountryOptions
+    {
+        public string DefaultCountry { get; set; } = "UA";
+
+        public string[] Countries { get; set; } = { };
+    }
+}
